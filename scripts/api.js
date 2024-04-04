8814d6402f3c7a56b47f6e5ffaf075b7a22df407
Sam May <samrmay@sas.upenn.edu>
2024-04-04 16:58:30 -0400

    test api call with template popup

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,11 @@
 // File for managing requests to external apis
+
+const API_URL = "https://openexchangerates.org/api/"
+const APP_ID = "19b25b2d511245769592c95daba1ecf4"
+
+async function get_rate() {
+    let res = await fetch(API_URL + `latest.json?app_id=${APP_ID}`)
+    return await res.json();
+};
+
+get_rate()
