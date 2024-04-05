a7116559064ef472b12bab361b41b83341673835
Sam May <samrmay@sas.upenn.edu>
2024-04-05 16:51:24 -0400

    Basic error handling for api call

--- scripts/api.js
+++ scripts/api.js
@@ -3,9 +3,15 @@
 const API_URL = "https://openexchangerates.org/api/"
 const APP_ID = "19b25b2d511245769592c95daba1ecf4"
 
-async function get_rate() {
-    let res = await fetch(API_URL + `latest.json?app_id=${APP_ID}`)
-    return await res.json();
-};
+async function get_rate(base) {
+    if (base == null || typeof base != "string" || base.length == 0) {
+        base = "USD";
+    }
+    let res = await fetch(API_URL + `latest.json?app_id=${APP_ID}&base=${base}`);
 
-get_rate()
+    if (res.ok) {
+        return await res.json();
+    } else {
+        console.error(`Could not request exchange rates from API: ${await res.text()}`);
+    }
+}
